d6cdcd1e2f73c25701b7c32fc633101db67680df
Mangachh <mangachh@gmail.com>
2022-04-04 12:31:59 +0200

    add3d filter

---
+++ tasca04/src/main/java/sprint4/tasca04/filters/ItFilter.java
@@ -0,0 +1,29 @@
+package sprint4.tasca04.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class ItFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        response.addHeader("It-Academy-Exercise", "Simple-Service");
+        filterChain.doFilter(request, response);
+
+    }
+
+}
